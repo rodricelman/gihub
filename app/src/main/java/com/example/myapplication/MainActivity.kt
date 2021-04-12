b9f1e82dd94d8d08b5294865f43870d47854361a
rodricelman <rodrigohack3@gmail.com>
2021-04-12 08:31:31 -0500

    XD

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var position=0
+    lateinit var questions: ArrayList<Question>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadQuestions()
+        setupViews()
+    }
+
+    private fun loadQuestions() {
+        questions=ArrayList()
+        var question: Question;
+         question=Question("es lima capital de peru",true);
+        questions.add(question)
+         question=Question("lima capital chile",false);
+        questions.add(question)
+         question=Question("es venmezuela capital de chile",false);
+        questions.add(question)
+         question=Question("es caracas capital de venezuela",true);
+        questions.add(question)
+         question=Question("es madrir capital de mexico",false);
+        questions.add(question)
+    }
+
+
+    fun setupViews(){
+        //val btnYes= findViewById<Button>(R.id.btnYes)
+        tvSentence.text=questions[0].sentence
+       btnYes.setOnClickListener{
+         if(questions[position].answer)
+           Toast.makeText(this,"correcto",Toast.LENGTH_LONG).show()
+           else     Toast.makeText(this,"incorrecto",Toast.LENGTH_LONG).show()
+
+
+       }
+        
+        btnNo.setOnClickListener{
+            if(!questions[position].answer)
+                Toast.makeText(this,"correcto",Toast.LENGTH_LONG).show()
+            else     Toast.makeText(this,"incorrecto",Toast.LENGTH_LONG).show()
+        }
+        btnNext.setOnClickListener{
+            position++;
+            tvSentence.text=questions[position].sentence
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Question.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+class Question(s: String, b: Boolean) {
+
+     val sentence: String = s
+    val answer: Boolean =b
+}
